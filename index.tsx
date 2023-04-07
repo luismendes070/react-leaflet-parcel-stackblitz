3eabcc0f0ff70fab119164fe25c54b9585445a91
luismendes070 <machadoluismendes@gmail.com>
2023-04-07 08:40:21 -0300

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -1,14 +1,27 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 
-import App from './App';
+function App() {
+  const [geojsonData, setGeojsonData] = useState<any>();
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/data/polygons.geojson');
+      const data = await response.json();
+      setGeojsonData(data);
+    }
+    fetchData();
+  }, []);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px" }}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {geojsonData && (
+        <GeoJSON data={geojsonData} />
+      )}
+    </MapContainer>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
